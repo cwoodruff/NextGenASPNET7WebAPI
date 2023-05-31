b458fb468609cd1d37d488ec0ca5dd7cadc429a0
Chris Woodruff <cwoodruff@live.com>
2023-05-31 06:52:38 -0400

    The initial checkin after adding the projects

---
+++ Chinook.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Chinook.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Chinook.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Chinook.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
